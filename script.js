f980a871e9d27eb262264e0706f0b25aed5d44c3
Oliver Craigie <ogc@live.ie>
2024-02-11 12:57:46 +0000

    Add currentTime so audio be played in succession

--- script.js
+++ script.js
@@ -9,5 +9,6 @@ window.addEventListener("click", function (e) {
 window.addEventListener("keydown", function (e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   if (audio === null) return;
+  audio.currentTime = 0;
   audio.play();
 });
