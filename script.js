e3506987663375957819674896430303f341ea2e
Oliver Craigie <ogc@live.ie>
2024-02-11 11:40:14 +0000

    Play audioA on keyboard button press

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ const A = document.getElementById("a");
 
 const audioA = new Audio("./sounds/clap.wav");
 
-A.addEventListener("click", clickA);
-
-function clickA() {
-  console.log(A);
+A.addEventListener("click", function clickA() {
   audioA.play();
-}
+});
+
+window.addEventListener("keydown", function (e) {
+  audioA.play();
+});
